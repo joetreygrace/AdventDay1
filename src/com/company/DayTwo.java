ab591e56db65121d40ac378e4043a552ede9922a
Trey Grace <joeggrace@gmail.com>
2021-12-02 20:34:00 -0500

    Day 2 Part 1

---
+++ src/com/company/DayTwo.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DayTwo {
+    private static int depth, horizontal;
+
+    public static void main(String[] args) throws IOException {
+        Files.lines(Paths.get("src/com/resources/day_2.txt")).map(s -> s.split(" ")).forEach(DayTwo::parse);
+        System.out.println(depth * horizontal);
+    }
+
+    private static void parse(String[] s) {
+        int amount = Integer.parseInt(s[1]);
+        switch (s[0]){
+            case "forward" -> horizontal += amount;
+            case "up" -> depth -= amount;
+            case "down" -> depth += amount;
+        }
+    }
+}
